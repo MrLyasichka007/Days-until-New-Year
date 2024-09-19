e88c078770c07c41bb169d6052c658f3f7c77306
User <mrfox474@gmail.com>
2024-09-19 13:00:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function daysUntilNewYear() {
+    var now = new Date();
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = now - start;
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    console.log('Days until New Year: ' + (365-day));
+}
+daysUntilNewYear();
